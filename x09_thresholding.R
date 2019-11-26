eb389ad14dbea8d6267be2891142b45078e4fd33
wrahool <mail@subhayan.com>
2019-11-26 16:27:13 -0600

    thresholding

--- x09_thresholding.R
+++ x09_thresholding.R
@@ -41,8 +41,67 @@ ggplot(obs.threshold.df, aes(x=tau, y=components)) +
   xlab("tau") +
   ylab("remaining nodes")
 
+# explore the results
+tau = 0.583
+
 observed.g = filtered.master.g
-tau = 0.585
+E(observed.g)$norm.wt = (E(observed.g)$shared_audience - min(E(observed.g)$shared_audience))/
+  (max(E(observed.g)$shared_audience) - min(E(observed.g)$shared_audience))
 
 observed.g = delete.edges(observed.g, which(E(observed.g)$norm.wt <= tau))
 observed.g = delete.vertices(observed.g, degree(observed.g) == 0)
+
+############################################
+
+# remove an NPR and redo thresholding
+
+load("04_RData/WT2.Rdata")
+
+to_remove = WT2[[2]]
+
+observed.g = filtered.master.g
+observed.g = delete.vertices(observed.g, which(V(observed.g)$name %in% to_remove))
+
+# do thresholding
+
+taus = seq(0, 1, by = 0.001)
+
+E(observed.g)$norm.wt = (E(observed.g)$shared_audience - min(E(observed.g)$shared_audience))/
+  (max(E(observed.g)$shared_audience) - min(E(observed.g)$shared_audience))
+
+rem.nodes = NULL
+n.components = NULL
+connected = NULL
+for(threshold in taus) {
+  observed.g = delete.edges(observed.g, which(E(observed.g)$norm.wt <= threshold))
+  observed.g = delete.vertices(observed.g, degree(observed.g) == 0)
+  rem.nodes = c(rem.nodes, vcount(observed.g))
+  n.components  = c(n.components, count_components(observed.g))
+  connected = c(connected, is_connected(observed.g))
+}
+
+obs.threshold.df = as.data.frame(cbind(taus, rem.nodes, n.components, connected))
+names(obs.threshold.df) = c("tau", "nodes", "components", "is_connected")
+
+# ggplot(obs.threshold.df, aes(x=tau, y=nodes)) + 
+#   geom_line() +
+#   xlab("tau") +
+#   ylab("remaining nodes")
+
+ggplot(obs.threshold.df, aes(x=tau, y=components)) + 
+  geom_line() +
+  xlab("tau") +
+  ylab("number of connected components")
+
+# explore the results
+tau = 0.069
+
+observed.g = filtered.master.g
+observed.g = delete.vertices(observed.g, which(V(observed.g)$name %in% to_remove))
+
+E(observed.g)$norm.wt = (E(observed.g)$shared_audience - min(E(observed.g)$shared_audience))/
+  (max(E(observed.g)$shared_audience) - min(E(observed.g)$shared_audience))
+
+observed.g = delete.edges(observed.g, which(E(observed.g)$norm.wt <= tau))
+observed.g = delete.vertices(observed.g, degree(observed.g) == 0)
+plot(observed.g)
